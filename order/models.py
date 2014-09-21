95e2beae828f6b1a9b93067ac51b551c8d1fe8d3
JoeyHill <cupojoe88@gmail.com>
2014-09-21 14:40:54 -0700

    added some models and started others"

---
+++ order/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from entry.models import Entry
+from Bonappetitemployee import Bonappetitemployee
+
+# Create your models here.
+class Order(models.Model):
+	orderid = models.IntegerField(db_column='OrderID', primary_key=True) # Field name made lowercase.
+	entryid = models.ForeignKey(Entry.EntryID, db_column='EntryID', blank=True, null=True) # Field name made lowercase.
+	namefirst = models.CharField(db_column='NameFirst', max_length=-1, blank=True) # Field name made lowercase.
+	namelast = models.CharField(db_column='NameLast', max_length=-1, blank=True) # Field name made lowercase.
+	authenticated = models.BooleanField(db_column='Authenticated') # Field name made lowercase.
+	processed = models.BooleanField(db_column='Processed') # Field name made lowercase.
+	processedbyid = models.ForeignKey(Bonappetitemployee, db_column='ProcessedByID', blank=True, null=True) # Field name made lowercase.
+	processedon = models.DateField(db_column='ProcessedOn', blank=True, null=True) # Field name made lowercase.
+	duedate = models.DateField(db_column='DueDate', blank=True, null=True) # Field name made lowercase.
+	modified = models.DateTimeField(db_column='Modified') # Field name made lowercase.
+	created = models.DateField(db_column='Created', blank=True, null=True) # Field name made lowercase.
+	class Meta:
+		managed = False
+		db_table = 'Order'
+
+	def __unicode__(self):
+		return self.orderid
--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from sacklunch import *
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,7 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sacklunch.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'sacklunch.views.home', name='home'),
-    # url(r'^order/',include('sacklunch.order.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^$','',name='createorder')),
+    
 )
