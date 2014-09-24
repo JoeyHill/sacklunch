208298e2232ec113b4fe8f6f2796695c53b784d4
Matthew Al-Sheikh <matthewalsheikh@gmail.com>
2014-09-23 19:18:04 -0700

    Styles and Config - add base variables, reconfigure compass

--- sacklunch/static/config.rb
+++ sacklunch/static/config.rb
@@ -15,6 +15,11 @@ javascripts_dir = "js"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
-preferred_syntax = :sass
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
