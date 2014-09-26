4aab689143cf7df1c9d35fb0b32c963a0da4f457
JoeyHill <cupojoe88@gmail.com>
2014-09-26 08:23:58 -0700

    update file structure again

--- entry/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from entry.models import Entry
-
-
-# Register your models here.
-admin.site.register(Entry)
--- order/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import datetime
-
-# Create your views here.
-def createorder(request)
-	now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
