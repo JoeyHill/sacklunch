574069989e5357b9516e0357932fe9d3663f78ef
JoeyHill <cupojoe88@gmail.com>
2014-10-08 12:20:19 -0700

    update sandwich materials and admin

--- sacklunch/sandwich/admin.py
+++ sacklunch/sandwich/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from models import *
 
 # Register your models here.
-admin.site.register(Sandwich)
+# admin.site.register(Sandwich)
+admin.site.register(swBread)
+admin.site.register(swMeat)
+admin.site.register(swCheese)
--- sacklunch/sandwich/models.py
+++ sacklunch/sandwich/models.py
@@ -1,14 +1,58 @@
 from django.db import models
+from sacklunch.order.models import Order
+
 
 # Create your models here.
+
+class swBread(models.Model):	
+	id = models.AutoField(db_column='swBreadID', primary_key=True) # Field name made lowercase.
+	description = models.CharField(db_column='Description', max_length=100)
+	
+	class Meta:
+		managed = False
+		db_table = 'swBread'
+		verbose_name = 'BreadType'
+
+	
+	def __unicode__(self):
+		return self.description
+
+class swMeat(models.Model):	
+	id = models.AutoField(db_column='swMeatID', primary_key=True) # Field name made lowercase.
+	description = models.CharField(db_column='Description', max_length=100)
+	
+	class Meta:
+		managed = False
+		db_table = 'swMeat'
+		verbose_name = 'MeatType'
+	
+	def __unicode__(self):
+		return self.description
+
+class swCheese(models.Model):	
+	id = models.AutoField(db_column='swCheeseID', primary_key=True) # Field name made lowercase.
+	description = models.CharField(db_column='Description', max_length=100)
+	
+	class Meta:
+		managed = False
+		db_table = 'swCheese'
+		verbose_name = 'CheeseType'
+	
+	def __unicode__(self):
+		return self.description
+
 class Sandwich(models.Model):	
 	sandwichid = models.AutoField(db_column='SandwichID', primary_key=True) # Field name made lowercase.
-	orderid = models.IntegerField(db_column='OrderID') # Field name made lowercase.
-	swbreadid = models.IntegerField(db_column='swBreadID') # Field name made lowercase.
-	swmeatid = models.IntegerField(db_column='swMeatID') # Field name made lowercase.
-	swcheeseid = models.IntegerField(db_column='swCheeseID') # Field name made lowercase.
+	orderid = models.ForeignKey(Order, db_column='OrderID') # Field name made lowercase.
+	swbreadid = models.ForeignKey(swBread, db_column='swBreadID') # Field name made lowercase.
+	swmeatid = models.ForeignKey(swMeat,db_column='swMeatID') # Field name made lowercase.
+	swcheeseid = models.ForeignKey(swCheese, db_column='swCheeseID') # Field name made lowercase.
+	
 	class Meta:
 		managed = False
 		db_table = 'Sandwich'
+		verbose_name_plural = 'Sandwiches'
+	
 	def __unicode__(self):
-		return str(self.sandwichid)
+		return str(self.orderid)
+
